6c3eeb6029dcbc88679867f38ae1322b328375e0
Dmitry S <dsagal+git@gmail.com>
2016-12-08 01:31:11 -0500

    Finish rearranging tests and fixtures

---
+++ tests/test_asttokens.py
@@ -0,0 +1,181 @@
+import ast
+import asttokens
+import astor    # pylint: disable=import-error
+import token
+import re
+import unittest
+from test_mark_tokens import read_fixture, collect_nodes_preorder
+
+
+def parse_snippet(text):
+  """
+  Returns the parsed AST tree for the given text, handling issues with indentation when text is
+  really an extracted part of larger code.
+  """
+  # If text is indented, it's a statement, and we need to put in a scope for indents to be valid
+  # (using textwrap.dedent is insufficient because some lines may not indented, e.g. comments or
+  # multiline strings).
+  indented = re.match(r'^[ \t]+\S', text)
+  if indented:
+    text = "def dummy():\n" + text
+
+  try:
+    module = ast.parse(text, 'exec')
+  except SyntaxError, e:
+    try:
+      # If we can't parse it, maybe we can parse the parenthesized version. This will be the case
+      # when the text is an expression that contains newlines but is missing enclosing parens.
+      module = ast.parse('(' + text + ')', 'exec')
+    except:
+      raise e
+
+  body = module.body[0]
+  return body.body[0] if indented else body
+
+
+class TestASTTokens(unittest.TestCase):
+
+  def test_tokenizing(self):
+    # Test that we produce meaningful tokens on initialization.
+    source = "import re  # comment\n\nfoo = 'bar'\n"
+    atok = asttokens.ASTTokens(source)
+    self.assertEqual(atok.text, source)
+    self.assertEqual([str(t) for t in atok.tokens], [
+      "NAME:'import'",
+      "NAME:'re'",
+      "COMMENT:'# comment'",
+      "NEWLINE:'\\n'",
+      "NL:'\\n'",
+      "NAME:'foo'",
+      "OP:'='",
+      'STRING:"\'bar\'"',
+      "NEWLINE:'\\n'",
+      "ENDMARKER:''"
+    ])
+
+    self.assertEqual(atok.tokens[5].type, token.NAME)
+    self.assertEqual(atok.tokens[5].string, 'foo')
+    self.assertEqual(atok.tokens[5].index, 5)
+    self.assertEqual(atok.tokens[5].startpos, 22)
+    self.assertEqual(atok.tokens[5].endpos, 25)
+
+  def test_token_methods(self):
+    # Test the methods that deal with tokens: prev/next_token, get_token, get_token_from_offset.
+    source = "import re  # comment\n\nfoo = 'bar'\n"
+    atok = asttokens.ASTTokens(source)
+    self.assertEqual(str(atok.tokens[3]), "NEWLINE:'\\n'")
+    self.assertEqual(str(atok.tokens[4]), "NL:'\\n'")
+    self.assertEqual(str(atok.tokens[5]), "NAME:'foo'")
+    self.assertEqual(str(atok.tokens[6]), "OP:'='")
+    self.assertEqual(atok.prev_token(atok.tokens[5]), atok.tokens[3])
+    self.assertEqual(atok.prev_token(atok.tokens[5], include_extra=True), atok.tokens[4])
+    self.assertEqual(atok.next_token(atok.tokens[5]), atok.tokens[6])
+    self.assertEqual(atok.next_token(atok.tokens[1]), atok.tokens[3])
+    self.assertEqual(atok.next_token(atok.tokens[1], include_extra=True), atok.tokens[2])
+
+    self.assertEqual(atok.get_token_from_offset(21), atok.tokens[4])
+    self.assertEqual(atok.get_token_from_offset(22), atok.tokens[5])
+    self.assertEqual(atok.get_token_from_offset(23), atok.tokens[5])
+    self.assertEqual(atok.get_token_from_offset(24), atok.tokens[5])
+    self.assertEqual(atok.get_token_from_offset(25), atok.tokens[5])
+    self.assertEqual(atok.get_token_from_offset(26), atok.tokens[6])
+
+    self.assertEqual(atok.get_token(2, 0), atok.tokens[4])
+    self.assertEqual(atok.get_token(3, 0), atok.tokens[5])
+    self.assertEqual(atok.get_token(3, 1), atok.tokens[5])
+    self.assertEqual(atok.get_token(3, 2), atok.tokens[5])
+    self.assertEqual(atok.get_token(3, 3), atok.tokens[5])
+    self.assertEqual(atok.get_token(3, 4), atok.tokens[6])
+
+    self.assertEqual(list(atok.token_range(atok.tokens[4], atok.tokens[6], include_extra=True)),
+                     atok.tokens[4:7])
+
+
+  def test_mark_tokens(self):
+    # There is a generic way to test it. We can take an arbitrary piece of code, parse it, and for
+    # each AST node, extract the corresponding code test. For nodes that are statements or
+    # expressions, that piece should itself be compilable, and the resulting AST node should be
+    # equivalent.
+    paths = [
+      'astroid/absimport.py',
+      'astroid/all.py',
+      'astroid/clientmodule_test.py',
+      'astroid/descriptor_crash.py',
+      'astroid/email.py',
+      'astroid/format.py',
+      'astroid/module.py',
+      'astroid/module2.py',
+      'astroid/noendingnewline.py',
+      'astroid/notall.py',
+      'astroid/recursion.py',
+      'astroid/suppliermodule_test.py',
+    ]
+    for path in paths:
+      source = read_fixture(path)
+      root = ast.parse(source)
+      atok = asttokens.ASTTokens(source)
+      atok.mark_tokens(root)
+
+      for node in ast.walk(root):
+        if not isinstance(node, (ast.stmt, ast.expr)):
+          continue
+        text = atok.get_text(node)
+        rebuilt_node = parse_snippet(text)
+
+        # Now we need to check if the two nodes are equivalent.
+        try:
+          self.assertEqual(astor.to_source(rebuilt_node), astor.to_source(node))
+        except AssertionError, e:
+          print "OUTPUT DIFFERS FOR:", text
+          raise
+
+  def test_deep_recursion(self):
+    # This testcase has 1050 strings joined with '+', which causes naive recursions to fail with
+    # 'maximum recursion depth exceeded' error.
+    source = read_fixture('astroid/joined_strings.py')
+    root = ast.parse(source)
+    atok = asttokens.ASTTokens(source)
+    atok.mark_tokens(root)
+
+    # We handle it find, but we can't use astor.to_source on it because astor chokes. So we check
+    # differently.
+    all_nodes = collect_nodes_preorder(root)
+    self.assertEqual(len(all_nodes), 2104)
+    self.assertEqual(atok.get_text(all_nodes[-1]),
+                     "'F1akOFFiRIgPHTZksKBAgMCLGTdGNIAAQgKfDAcgZbj0odOnUA8GBAA7'")
+    self.assertEqual(atok.get_text(all_nodes[-2]),
+                     "'Ii0uLDAxLzI0Mh44U0gxMDI5JkM0JjU3NDY6Kjc5Njo7OUE8Ozw+Oz89QTxA'")
+    self.assertEqual(atok.get_text(all_nodes[1053]),
+                     "'R0lGODlhigJnAef/AAABAAEEAAkCAAMGAg0GBAYJBQoMCBMODQ4QDRITEBkS'")
+    self.assertEqual(atok.get_text(all_nodes[1052]),
+                     "'R0lGODlhigJnAef/AAABAAEEAAkCAAMGAg0GBAYJBQoMCBMODQ4QDRITEBkS'\r\n" +
+                     "     +'CxsSEhkWDhYYFQ0aJhkaGBweGyccGh8hHiIkIiMmGTEiHhQoPSYoJSkqKDcp'")
+    assign = next(n for n in all_nodes if isinstance(n, ast.Assign))
+    self.assertTrue(atok.get_text(assign).startswith("x = ("))
+    self.assertTrue(atok.get_text(assign).endswith(")"))
+
+    binop = next(n for n in all_nodes if isinstance(n, ast.BinOp))
+    self.assertTrue(atok.get_text(binop).startswith("'R0l"))
+    self.assertTrue(atok.get_text(binop).endswith("AA7'"))
+
+  def test_print_function(self):
+    # This testcase imports print as function (using from __future__). Check that we can parse.
+    source = read_fixture('astroid/nonregr.py')
+    root = ast.parse(source)
+    atok = asttokens.ASTTokens(source)
+    atok.mark_tokens(root)
+    all_nodes = collect_nodes_preorder(root)
+
+    def get_node_text(line, col, type_name):
+      token = atok.get_token(line, col)
+      for n in all_nodes:
+        if n.first_token == token and n.__class__.__name__ == type_name:
+          return atok.get_text(n)
+
+    # Line 16 is: [indent 8] print(v.get('yo'))
+    self.assertEqual(get_node_text(16, 8, 'Call'), "print(v.get('yo'))")
+    self.assertEqual(get_node_text(16, 14, 'Attribute'), 'v.get')
+    self.assertEqual(get_node_text(16, 14, 'Call'), "v.get('yo')")
+
+if __name__ == "__main__":
+  unittest.main()
---
+++ tests/test_mark_tokens.py
@@ -0,0 +1,99 @@
+import ast
+import asttokens
+import os
+import unittest
+
+def get_fixture_path(*path_parts):
+  return os.path.join(os.path.dirname(__file__), "fixtures", *path_parts)
+
+def read_fixture(*path_parts):
+  with open(get_fixture_path(*path_parts), "rb") as f:
+    return f.read()
+
+def collect_nodes_preorder(root):
+  nodes = []
+  def append(node, par_value):
+    nodes.append(node)
+    return (None, None)
+  asttokens.util.visit_tree(root, append, None)
+  return nodes
+
+class TestMarkTokens(unittest.TestCase):
+
+  def test_assign_first_tokens(self):
+    source = read_fixture('astroid', 'module.py')
+    root = ast.parse(source)
+    atok = asttokens.ASTTokens(source)
+    atok.mark_tokens(root)
+    all_nodes = collect_nodes_preorder(root)
+
+    def get_node_types_at(line, col):
+      token = atok.get_token(line, col)
+      return {n.__class__.__name__ for n in all_nodes if n.first_token == token}
+
+    # Line 14 is: [indent 4] MY_DICT[key] = val
+    self.assertEqual(get_node_types_at(14, 4), {'Name', 'Subscript', 'Assign'})
+
+    # Line 35 is: [indent 12] raise XXXError()
+    self.assertEqual(get_node_types_at(35, 12), {'Raise'})
+    self.assertEqual(get_node_types_at(35, 18), {'Call', 'Name'})
+
+    # Line 53 is: [indent 12] autre = [a for (a, b) in MY_DICT if b]
+    self.assertEqual(get_node_types_at(53, 20), {'ListComp'})
+    self.assertEqual(get_node_types_at(53, 21), {'Name'})
+    self.assertEqual(get_node_types_at(53, 23), {'comprehension'})
+
+    # Line 59 is: [indent 12] global_access(local, val=autre)
+    self.assertEqual(get_node_types_at(59, 12), {'Name', 'Call', 'Expr'})
+    self.assertEqual(get_node_types_at(59, 26), {'Name'})
+    self.assertEqual(get_node_types_at(59, 37), {'Name', 'keyword'})
+
+  def test_mark_tokens_simple(self):
+    source = read_fixture('astroid', 'module.py')
+    root = ast.parse(source)
+    atok = asttokens.ASTTokens(source)
+    atok.mark_tokens(root)
+    all_nodes = collect_nodes_preorder(root)
+
+    def get_node_text(line, col, type_name):
+      token = atok.get_token(line, col)
+      for n in all_nodes:
+        if n.first_token == token and n.__class__.__name__ == type_name:
+          return atok.get_text(n)
+
+    # Line 14 is: [indent 4] MY_DICT[key] = val
+    self.assertEqual(get_node_text(14, 4, 'Name'), 'MY_DICT')
+    self.assertEqual(get_node_text(14, 4, 'Subscript'), 'MY_DICT[key]')
+    self.assertEqual(get_node_text(14, 4, 'Assign'), 'MY_DICT[key] = val')
+
+    # Line 35 is: [indent 12] raise XXXError()
+    self.assertEqual(get_node_text(35, 12, 'Raise'), 'raise XXXError()')
+    self.assertEqual(get_node_text(35, 18, 'Call'), 'XXXError()')
+    self.assertEqual(get_node_text(35, 18, 'Name'), 'XXXError')
+
+    # Line 53 is: [indent 12] autre = [a for (a, b) in MY_DICT if b]
+    self.assertEqual(get_node_text(53, 20, 'ListComp'), '[a for (a, b) in MY_DICT if b]')
+    self.assertEqual(get_node_text(53, 21, 'Name'), 'a')
+
+  def test_mark_tokens_multiline(self):
+    source = (
+"""(    # line1
+a,      # line2
+b +     # line3
+  c +   # line4
+  d     # line5
+)""")
+    root = ast.parse(source)
+    atok = asttokens.ASTTokens(source)
+    atok.mark_tokens(root)
+
+    all_nodes = {atok.get_text(node) for node in ast.walk(root)}
+    self.assertEqual(all_nodes, {
+      None,           # nodes we don't care about
+      source,
+      'a', 'b', 'c', 'd',
+      # All other expressions preserve newlines and comments but are parenthesized.
+      'b +     # line3\n  c',
+      'b +     # line3\n  c +   # line4\n  d',
+      'a,      # line2\nb +     # line3\n  c +   # line4\n  d',
+    })
