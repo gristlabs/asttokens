7f9ad179fdc1be82125980298b59026dabef4125
Dmitry S <dsagal+git@gmail.com>
2018-07-07 23:56:58 -0400

    Bump patch version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
   name='asttokens',
-  version='1.1.10',
+  version='1.1.11',
   description='Annotate AST trees with source code positions',
   long_description=long_description,
   url='https://github.com/gristlabs/asttokens',
