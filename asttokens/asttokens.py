e6791de37f2fbcf43099fbf1dfc5700458c3af7c
Tom Parker-Shemilt <palfrey@tevp.net>
2022-01-22 13:16:49 +0000

    Reset ASTTokens source_text conversion

--- asttokens/asttokens.py
+++ asttokens/asttokens.py
@@ -51,7 +51,7 @@ class ASTTokens(object):
     # Decode source after parsing to let Python 2 handle coding declarations.
     # (If the encoding was not utf-8 compatible, then even if it parses correctly,
     # we'll fail with a unicode error here.)
-    source_text = six.ensure_text(source_text)
+    source_text = six.ensure_str(source_text)
 
     self._text = source_text
     self._line_numbers = LineNumbers(source_text)
@@ -86,7 +86,8 @@ class ASTTokens(object):
     """
     # This is technically an undocumented API for Python3, but allows us to use the same API as for
     # Python2. See http://stackoverflow.com/a/4952291/328565.
-    for index, tok in enumerate(tokenize.generate_tokens(io.StringIO(text).readline)):
+    file_obj = io.StringIO(six.ensure_text(text))
+    for index, tok in enumerate(tokenize.generate_tokens(lambda: file_obj.readline())):
       tok_type, tok_str, start, end, line = tok
       yield Token(tok_type, tok_str, start, end, line, index,
                   self._line_numbers.line_to_offset(start[0], start[1]),
