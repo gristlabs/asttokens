d4b32a8bea66639c5b3b99b2060ff024a5c7deb3
Alex Hall <alex.mojaki@gmail.com>
2022-10-01 14:04:51 +0200

    Add test_unmarked.py

---
+++ tests/test_unmarked.py
@@ -0,0 +1,46 @@
+import ast
+import unittest
+
+from asttokens import asttokens, supports_unmarked
+
+source = """
+x = 1
+if x > 0:
+  for i in range(10):
+    print(i)
+else:
+  print("negative")
+"""
+
+
+@unittest.skipUnless(supports_unmarked(), "Python version does not support unmarked nodes")
+class TestUmarked(unittest.TestCase):
+  def test_unmarked(self):
+    atok = asttokens.ASTTokens(source, parse=True, init_tokens=False)
+    for node in ast.walk(atok.tree):
+      self.assertTrue(supports_unmarked(node), node)
+
+      if not hasattr(node, 'lineno'):
+        self.assertEqual(ast.get_source_segment(source, node), None)
+        self.assertEqual(atok.get_text(node), source if isinstance(node, ast.Module) else '')
+        continue
+
+      self.assertEqual(
+        atok.get_text_positions(node, padded=False),
+        (
+          (node.lineno, node.col_offset),
+          (node.end_lineno, node.end_col_offset),
+        ),
+      )
+
+      for padded in [True, False]:
+        self.assertEqual(
+          atok.get_text(node, padded=padded),
+          ast.get_source_segment(source, node, padded=padded),
+          node
+        )
+
+    self.assertIsNone(atok._tokens)
+    self.assertIsNone(atok._token_offsets)
+    with self.assertRaises(AssertionError):
+      getattr(atok, 'tokens')
